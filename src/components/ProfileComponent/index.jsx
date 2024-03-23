4d30b568e6e274f07aea6b46af07f90c26cf44cf
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-03-22 22:45:47 -0300

    [M03S08] Ex 5 - Teste de renderização - texto

---
+++ src/components/ProfileComponent/index.jsx
@@ -0,0 +1,11 @@
+function ProfileComponent({ titulo, subtitulo }) {
+
+  return (
+    <div>
+      <h3>{titulo}</h3>
+      <h4>{subtitulo}</h4>
+    </div>
+  )
+}
+
+export default ProfileComponent;
---
+++ src/tests/Components/ProfileComponent.test.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import { render, screen } from '@testing-library/react'
+import ProfileComponent from '../../components/ProfileComponent'
+
+describe('ProfileComponent', () => {
+  test('Validando teste de integração passando parametros', () => {
+    render(<ProfileComponent titulo="Lorem Ipsum" subtitulo="Lorem ipsum dolor sit amet" />)
+    expect(screen.getByText('Lorem Ipsum')).toBeInTheDocument();
+    expect(screen.getByText('Lorem ipsum dolor sit amet')).toBeInTheDocument();
+  })
+})
